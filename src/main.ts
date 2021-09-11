dc013257f554b980cf0fb147f129495e3a5483fb
Alex <17277800+alex-schaaf@users.noreply.github.com>
2021-09-11 13:04:37 +0200

    ✨ add default width, heigth, set HSL as default color mode

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import p5 from "p5"
 import "./styles.scss"
 
+const width = 800
+const height = width * Math.sqrt(2)
 
 const sketch = (p: p5) => {
   p.setup = () => {
-    const canvas = p.createCanvas(window.innerWidth, window.innerHeight)
+    const canvas = p.createCanvas(width, height)
     canvas.parent("p5-canvas")
+    p.colorMode(p.HSL)
+    p.stroke(360, 100, 100, 1)
   }
 
   p.draw = () => {}
